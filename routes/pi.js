35a128de004f4ad89f08c76b4a6acc0242c87aeb
Kyle Brandon <kyle@brandfam.net>
2012-11-21 20:03:22 -0600

    try write

--- routes/pi.js
+++ routes/pi.js
@@ -5,34 +5,23 @@
 var gpio = require("pi-gpio");
 
 exports.on = function(req, res){
-  gpio.open(11, "out", function(err){
+
+  gpio.write(11, 1, function(err){
     if (err){
       res.send("ERROR");
     }
-
-    gpio.write(11, 1, function(err){
-      if (err){
-        res.send("ERROR");
-      }
-    });
-    res.send("ON");
-  })
+  });
+  res.send("ON");
 
 };
 
 exports.off = function(req, res){
 
-  gpio.open(11, "out", function(err){
+  gpio.write(11, 0, function(err){
     if (err){
       res.send("ERROR");
     }
-
-    gpio.write(11, 0, function(err){
-      if (err){
-        res.send("ERROR");
-      }
-     });
-    res.send("OFF");
-  })
+   });
+  res.send("OFF");
 
 };
